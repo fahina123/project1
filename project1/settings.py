0ccf1a84aa7f922a664f02286e3489951a40be83
FAHINA <fahinaajmal@gmail.com>
2022-04-14 13:23:34 +0530

    102th commit

--- project1/settings.py
+++ project1/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static/')
 ]
 
-STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifeStstaticFilesStorage'
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
